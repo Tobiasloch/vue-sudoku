5c5cf5c4dbbad693a72466c1ed6c51921fd3fcaa
Tobias loch <Tobias.loch@mail.de>
2024-06-03 14:04:44 +0200

    Adding typescript via vue cli

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>
-    <SudokuComponent></SudokuComponent>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
-<script setup>
-import SudokuComponent from '@/components/SudokuComponent.vue';
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+  },
+});
 </script>
