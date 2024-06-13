1f593455b508cad58624e1c242c60b627809dcd4
Tobias loch <Tobias.loch@mail.de>
2024-06-13 17:32:34 +0200

    initially the sudoku is now empty and the generator dialog is shown cleanup of areYouSureDialog code to make it more readable

--- src/views/SudokuView.vue
+++ src/views/SudokuView.vue
@@ -33,28 +33,28 @@
 
                 <v-tooltip text="Solve" location="top">
                     <template v-slot:activator="{ props }">
-                        <v-btn icon @click="initSolveQuestion()" v-bind="props">
+                        <v-btn icon @click="areYouSureObject = areYouSureItems[2]; areYouSureDialog = true" v-bind="props">
                             <v-icon>mdi-check</v-icon>
                         </v-btn>
                     </template>
                 </v-tooltip>
                 <v-tooltip text="Clear" location="top">
                     <template v-slot:activator="{ props }">
-                        <v-btn icon @click="initClearQuestion()" v-bind="props">
+                        <v-btn icon @click="areYouSureObject = areYouSureItems[1]; areYouSureDialog = true" v-bind="props">
                             <v-icon>mdi-delete</v-icon>
                         </v-btn>
                     </template>
                 </v-tooltip>
                 <v-tooltip text="Reset" location="top">
                     <template v-slot:activator="{ props }">
-                        <v-btn icon @click="initResetQuestion()" v-bind="props">
+                        <v-btn icon @click="areYouSureObject = areYouSureItems[0]; areYouSureDialog = true" v-bind="props">
                             <v-icon>mdi-reload</v-icon>
                         </v-btn>
                     </template>
                 </v-tooltip>
 
                 <v-dialog v-model="areYouSureDialog" max-width="400">
-                    <v-card :prepend-icon="areYouSureIcon" :text="areYouSureText" :title="areYouSureTitle">
+                    <v-card :prepend-icon="areYouSureObject.icon" :text="areYouSureObject.text" :title="areYouSureObject.title">
                         <template v-slot:actions>
                             <v-spacer></v-spacer>
 
@@ -62,15 +62,23 @@
                                 No
                             </v-btn>
 
-                            <v-btn color="primary" @click="areYouSureDialog = false; areYouSureAction()">
+                            <v-btn color="primary" @click="areYouSureDialog = false; areYouSureObject.action()">
                                 Yes
                             </v-btn>
                         </template>
                     </v-card>
                 </v-dialog>
 
-                <v-dialog v-model="sudokuGeneratorDialog" max-width="600">
-                    <v-card :loading="generatorIterations > 0" prepend-icon="mdi-shuffle" title="Generate new sudoku?">
+                <v-dialog 
+                    v-model="sudokuGeneratorDialog" 
+                    max-width="600"
+                    :persistent="generatorIterations > 0"
+                >
+                    <v-card 
+                        :loading="generatorIterations > 0" 
+                        prepend-icon="mdi-shuffle" 
+                        title="Generate new sudoku?"
+                    >
                         <template v-slot:loader="{ isActive }">
                             <v-progress-linear
                                 :active="isActive"
@@ -118,7 +126,7 @@
                         <template v-slot:actions>
                             <v-spacer></v-spacer>
 
-                            <v-btn @click="sudokuGeneratorDialog = false;generatorIterations = sudokuDifficulty">
+                            <v-btn @click="sudokuGeneratorDialog = false;">
                                 Cancel
                             </v-btn>
 
@@ -141,67 +149,23 @@ import { ref } from 'vue';
 const sudokuComponent = ref(null);
 
 // have a nearly solved sudoku board as initial state with only 5 cells left to solve
-const initialState = [
-    [0, 2, 5, 7, 6, 3, 8, 4, 9],
-    [4, 7, 8, 5, 1, 9, 3, 6, 2],
-    [9, 6, 3, 8, 2, 4, 5, 7, 1],
-    [6, 5, 1, 9, 4, 2, 7, 3, 8],
-    [8, 9, 4, 3, 7, 5, 2, 1, 6],
-    [2, 3, 7, 6, 8, 1, 9, 5, 4],
-    [7, 1, 9, 2, 3, 6, 4, 8, 5],
-    [5, 8, 6, 4, 9, 7, 1, 2, 3],
-    [3, 4, 2, 1, 5, 8, 6, 9, 0]
-];
-let sudoku = ref(new Sudoku(
-    //     [
-    //   [0, 3, 0, 0, 0, 0, 0, 0, 0],
-    //   [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    //   [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    //   [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    //   [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    //   [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    //   [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    //   [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    //   [0, 0, 0, 0, 8, 0, 0, 7, 9]
-    // ]
-    initialState
-))
+let sudoku = ref(new Sudoku())
 let generatedSudoku = sudoku.value.copy();
 
-
-function initSolveQuestion() {
-    areYouSureText.value = 'Are you sure you want to run the solver to completely solve the sudoku for you?';
-    areYouSureTitle.value = 'Solver';
-    areYouSureIcon.value = 'mdi-check';
-    areYouSureAction.value = () => sudokuComponent.value.solve();
-    areYouSureDialog.value = true;
-}
-
-function initClearQuestion() {
-    areYouSureText.value = 'Are you sure you want to clear the sudoku, such that all fields are empty?';
-    areYouSureTitle.value = 'Clear';
-    areYouSureIcon.value = 'mdi-delete';
-    areYouSureAction.value = () => {
-        sudoku.value = new Sudoku();
-    };
-    areYouSureDialog.value = true;
-}
-
-function initResetQuestion() {
-    areYouSureText.value = 'Are you sure you want to reset the state of the  sudoku to the generated one?';
-    areYouSureTitle.value = 'Reset';
-    areYouSureIcon.value = 'mdi-reload';
-    areYouSureAction.value = () => {
+const areYouSureItems = ref([
+    { title:'Reset', text: 'Are you sure you want to reset the state of the sudoku to the generated one?', icon: 'mdi-reload', action: () => {
         sudoku.value = generatedSudoku.copy();
-    };
-    areYouSureDialog.value = true;
-}
+    }},
+    { title:'Clear', text: 'Are you sure you want to clear the sudoku, such that all fields are empty?', icon: 'mdi-delete', action: () => {
+        sudoku.value = new Sudoku();
+    }},
+    { title:'Solve', text: 'Are you sure you want to run the solver to completely solve the sudoku for you?', icon: 'mdi-check', action: () => {
+        sudokuComponent.value.solve();
+    } },
+]);
 
 let areYouSureDialog = ref(false);
-let areYouSureText = ref('');
-let areYouSureTitle = ref('');
-let areYouSureIcon = ref('');
-let areYouSureAction = ref(() => { });
+let areYouSureObject = ref(null);
 
 const difficultyOptions = ref([
     { label: "Easy", value: 43 },
@@ -224,7 +188,7 @@ function updatedDifficulty() {
     }
 }
 
-let sudokuGeneratorDialog = ref(false);
+let sudokuGeneratorDialog = ref(true);
 
 async function iterateGenerator() {
     return new Promise((resolve) => {
@@ -232,7 +196,7 @@ async function iterateGenerator() {
             resolve()
         }, 0)
     }).then(() => {
-        if (generatorIterations.value < sudokuDifficulty.value) {
+        if (generatorIterations.value < sudokuDifficulty.value && sudokuGeneratorDialog.value) {
             generatedSudoku.clearCell(true, true)
             generatorIterations.value++
 
